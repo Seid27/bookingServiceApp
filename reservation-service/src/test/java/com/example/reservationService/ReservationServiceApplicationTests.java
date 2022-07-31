d28f9cd46fb1d598ef7952764043fd60dfe3427c
Seid Redi <seidredi7@gmail.com>
2022-07-31 09:52:51 -0500

    added a remote config server properties file

--- reservation-service/src/test/java/com/example/reservationService/ReservationServiceApplicationTests.java
+++ reservation-service/src/test/java/com/example/reservationService/ReservationServiceApplicationTests.java
@@ -1,6 +1,8 @@
 package com.example.reservationService;
 
+import org.jasypt.encryption.StringEncryptor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
